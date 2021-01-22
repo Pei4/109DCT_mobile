9d1f1dd14f5f3d96fd177cad4aa9bf93e59b63f7
Pei4 <40812695+Pei4@users.noreply.github.com>
2021-01-22 16:30:35 +0800

    move to html

--- public/js/functions.js
+++ public/js/functions.js
@@ -76,35 +76,7 @@ function btnCheck(){
     showSth('me');
     hideSth('planet');
     document.querySelector('#main').style.display = 'none';
-    //instacan
-    let scanner = new Instascan.Scanner({ video: document.getElementById('preview') });
-    scanner.addListener('scan', function (content) {
-        console.log(content);
-    });
-    Instascan.Camera.getCameras().then(function (cameras) {
-        if (cameras.length > 0) {
-            scanner.start(cameras[0]);
-        } else {
-            console.error('No cameras found.');
-        }
-    }).catch(function (e) {
-        console.error(e);
-    });
-}
 
-//instacan
-/*let scanner = new Instascan.Scanner({
-    video: document.getElementById('preview')
-});
-scanner.addEventListener()('scan', function(content) {
-    console.log(content);
-});
-Instascan.Camera.getCameras().then(function(cameras) {
-    if (cameras.length > 0) {
-        scanner.start(cameras[0]);
-    } else {
-        console.error('No cameras found.');
-    }
-}).catch(function(e) {
-    console.error(e);
-});*/
+    //instacan
+    /**/
+}
