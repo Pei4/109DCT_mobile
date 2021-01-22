9e66c7a76fa557ad94d4befb845432bbfbf64b30
Pei4 <40812695+Pei4@users.noreply.github.com>
2021-01-22 16:54:42 +0800

    semicolumn

--- public/js/functions.js
+++ public/js/functions.js
@@ -55,7 +55,7 @@ function dialogControl(){
     if(checkpoint == 1){
         showSth('lv1Btn');
         disableSth('nextBtn');
-    }
+    };
     if(meCont.includes(checkpoint)){
         meDialogNum ++;
         changeHtml('me',meDialogArray[meDialogNum]);
@@ -67,7 +67,7 @@ function dialogControl(){
         changeHtml('planet',planetDialogArray[planetDialogNum]);
         showSth('planet');
         hideSth('me');
-    }
+    };
 }
 
 function btnCheck(){
