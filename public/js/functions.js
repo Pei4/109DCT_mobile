c906a23c3bcb2d63df8a7e004a4e4df49669a4c5
Pei4 <40812695+Pei4@users.noreply.github.com>
2021-01-25 16:33:19 +0800

    set alert

--- public/js/functions.js
+++ public/js/functions.js
@@ -77,5 +77,22 @@ function btnCheck(){
     showSth('insta');
     hideSth('planet');
     hideSth('main');
-
+    //instacan
+    let scanner = new Instascan.Scanner({
+        video: document.getElementById('preview')
+    });
+    scanner.addListener('scan', function (content) {
+        alert(content);
+    });
+    Instascan.Camera.getCameras().then(function (cameras) {
+        if (cameras.length > 0) {
+            scanner.start(cameras[0]);
+            alert('yes camera');
+        }
+        else {
+            alert('No cameras found.');
+        }
+    }).catch(function (e) {
+        alert(e);
+    });
 }
