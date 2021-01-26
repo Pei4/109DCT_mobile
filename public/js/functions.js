9c06bfc1f046c2d0c6b7f86d50a278e641068a57
Pei4 <40812695+Pei4@users.noreply.github.com>
2021-01-26 13:25:01 +0800

    try continued true

--- public/js/functions.js
+++ public/js/functions.js
@@ -78,7 +78,7 @@ function btnCheck(){
     hideSth('main');
     document.getElementById('preview').play();
     //instacan
-    let scanner = new Instascan.Scanner({video: document.getElementById('preview')});
+    let scanner = new Instascan.Scanner({video: document.getElementById('preview'),continuous: true});
     alert(scanner.video);
     scanner.addListener('scan', function (content) {
         alert(content);
