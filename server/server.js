10e321887d5d0a365927b1616b09b72f8c7be3da
Pei4 <pei4.cats@gmail.com>
2021-02-12 14:58:21 +0800

    remove server

--- server/server.js
+++
@@ -1,6 +0,0 @@
-const handler = require('serve-handler');
-const http = require('http');
-const server = http.createServer((request, response) => {
-    return handler(request, response, {public: "public"});
-});
-const io = require('socket.io')(server);
