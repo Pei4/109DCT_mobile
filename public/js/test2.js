9b788a9b25e8419759add50530565c1c7fd18a0c
Pei4 <pei4.cats@gmail.com>
2021-03-02 16:28:08 +0800

    try CORS

--- public/js/test2.js
+++ public/js/test2.js
@@ -1,10 +1,11 @@
+window.googleDocCallback = function () { return true; };
 function send() {
     console.log("go");
     let test1 = document.querySelector('#t1').value;
     let test2 = document.querySelector('#t2').value;
     $.ajax({
         type: "post",
-        url: "https://script.google.com/macros/s/AKfycbxczugTBSFn2zVudV0D0rf61q7o7WMJ7UVBfAYtZIzsu-aqry0_udg9LA/exec",
+        url: "https://script.google.com/macros/s/AKfycbxczugTBSFn2zVudV0D0rf61q7o7WMJ7UVBfAYtZIzsu-aqry0_udg9LA/exec?callback=googleDocCallback",
         data: {
             "test1": test1,
             "test2": test2
