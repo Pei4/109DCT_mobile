163e96c52c58808b1067a4519a54435847547098
Pei4 <pei4.cats@gmail.com>
2021-03-02 16:31:41 +0800

    DoGet

--- public/js/test2.js
+++ public/js/test2.js
@@ -4,7 +4,7 @@ function send() {
     let test1 = document.querySelector('#t1').value;
     let test2 = document.querySelector('#t2').value;
     $.ajax({
-        type: "post",
+        type: "get",
         url: "https://script.google.com/macros/s/AKfycbxczugTBSFn2zVudV0D0rf61q7o7WMJ7UVBfAYtZIzsu-aqry0_udg9LA/exec?callback=googleDocCallback",
         data: {
             "test1": test1,
