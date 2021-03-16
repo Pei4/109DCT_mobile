f5b07da94be9fd3355e62b79222eb673196cb898
Pei4 <pei4.cats@gmail.com>
2021-03-16 14:13:56 +0800

    try video

--- public/js/lv3.js
+++ public/js/lv3.js
@@ -1,3 +1,17 @@
+var video = document.getElementById('preview');
+var canvas = document.getElementById('shotCanvas');
+var context = canvas.getContext('2d');
+//var w, h, ratio;
+
+//影片比例
+/*video.addEventListener('loadedmetadata', function() {
+    ratio = video.videoWidth / video.videoHeight;
+    w = video.videoWidth - 100;
+    h = parseInt(w / ratio, 10);
+    canvas.width = w;
+    canvas.height = h;
+}, false);*/
+
 function runVid(){
     let constraints = {video: {facingMode: { exact: "environment" }}};
     let video = document.querySelector('#preview');
@@ -18,19 +32,23 @@ function runVid(){
 //截圖（拍照）
 function screenshot(){
     html2canvas(document.getElementById('capture')).then(function(canvas) {
+        context.fillRect(0, 0, w, h);
+        context.drawImage(video, 0, 0, w, h);
         //iOS
-        imageURL = canvas.toDataURL();
+        /*imageURL = canvas.toDataURL();
         let img = document.getElementById('screenShotImg');
         img.crossOrigin = "Anonymous";
         //img.id = "getshot";
         img.src = imageURL;
-        document.getElementById('capture').appendChild(img);
+        document.getElementById('capture').appendChild(img);*/
+
         //normal
         /*document.getElementById('capture').appendChild(canvas);
         var a = document.createElement('a');
         a.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
         a.download = 'image.jpg';
         a.click();*/
+
         //下載
         /*
         let a = document.createElement("a");
