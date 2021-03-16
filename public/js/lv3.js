d6e1e77cbea41bb684040261db9f27a7ee4a3e44
Pei4 <pei4.cats@gmail.com>
2021-03-16 10:10:24 +0800

    try runVid

--- public/js/lv3.js
+++ public/js/lv3.js
@@ -1,17 +1,19 @@
-let constraints = {video: {facingMode: { exact: "environment" }}};
-let video = document.querySelector('#preview');
+function runVid(){
+    let constraints = {video: {facingMode: { exact: "environment" }}};
+    let vid = document.querySelector('#preview');
+    //存取相機
+    function handleSuccess(stream) {
+        window.stream = stream;
+        vid.srcObject = stream;
+    }
+    function handleError(error) {
+        console.log('getUserMedia error: ', error);
+    }
+    navigator.mediaDevices.getUserMedia(constraints)
+        .then(handleSuccess)
+        .catch(handleError)
+}
 
-//存取相機
-function handleSuccess(stream) {
-    window.stream = stream;
-    video.srcObject = stream;
-}
-function handleError(error) {
-    console.log('getUserMedia error: ', error);
-}
-navigator.mediaDevices.getUserMedia(constraints)
-    .then(handleSuccess)
-    .catch(handleError)
 
 //截圖（拍照）
 function screenshot(){
