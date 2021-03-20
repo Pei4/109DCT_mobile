c04f5a5fdc1e2c8ee0f18e4844ffad5d0914df09
Pei4 <pei4.cats@gmail.com>
2021-03-20 11:03:54 +0800

    add window onload

--- public/js/lv3.js
+++ public/js/lv3.js
@@ -26,36 +26,38 @@ function runVid(){
         .then(handleSuccess)
         .catch(handleError)
 }
+window.onload = function (){
+    //截圖（拍照）
+    function screenshot(){
+        //影片比例
+        context.fillRect(0, 0, w, h);
+        context.drawImage(vid, 0, 0, w, h);
 
-//截圖（拍照）
-function screenshot(){
-    //影片比例
-    context.fillRect(0, 0, w, h);
-    context.drawImage(vid, 0, 0, w, h);
+        //html2canvas(document.getElementById('capture')).then(function(canvas) {
+        //iOS
+        /*imageURL = canvas.toDataURL();
+        let img = document.getElementById('screenShotImg');
+        img.crossOrigin = "Anonymous";
+        //img.id = "getshot";
+        img.src = imageURL;
+        document.getElementById('capture').appendChild(img);*/
 
-    //html2canvas(document.getElementById('capture')).then(function(canvas) {
-    //iOS
-    /*imageURL = canvas.toDataURL();
-    let img = document.getElementById('screenShotImg');
-    img.crossOrigin = "Anonymous";
-    //img.id = "getshot";
-    img.src = imageURL;
-    document.getElementById('capture').appendChild(img);*/
+        //normal
+        /*document.getElementById('capture').appendChild(canvas);
+        var a = document.createElement('a');
+        a.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
+        a.download = 'image.jpg';
+        a.click();*/
 
-    //normal
-    /*document.getElementById('capture').appendChild(canvas);
-    var a = document.createElement('a');
-    a.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
-    a.download = 'image.jpg';
-    a.click();*/
-
-    //下載
-    /*
-    let a = document.createElement("a");
-    a.href = getshot.src;
-    a.download = "workout_log.png";
-    a.click();
-    document.getElementById('capture').removeChild(img);*/
-    //});
+        //下載
+        /*
+        let a = document.createElement("a");
+        a.href = getshot.src;
+        a.download = "workout_log.png";
+        a.click();
+        document.getElementById('capture').removeChild(img);*/
+        //});
+    }
 }
+
 
