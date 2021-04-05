5eeb1ff697e98de3ac235d25fe52f0a4b4e9e38d
Pei4 <pei4.cats@gmail.com>
2021-04-05 21:03:35 +0800

    remove callback

--- public/js/lv0.js
+++ public/js/lv0.js
@@ -3,7 +3,7 @@ let num = 1;
 function read(){
     $.ajax({
         type: "post",
-        url: "https://script.google.com/macros/s/AKfycbxd4UmVaIwRdI9nUXst8gZ-8rwgss7WzUHuVyf1m6gVwEBM-ntdGjMo5p5_WBi7fV1k7w/exec?callback=googleDocCallback",
+        url: "https://script.google.com/macros/s/AKfycbxd4UmVaIwRdI9nUXst8gZ-8rwgss7WzUHuVyf1m6gVwEBM-ntdGjMo5p5_WBi7fV1k7w/exec",
         data: {
             "method":"read",
             "num":num
