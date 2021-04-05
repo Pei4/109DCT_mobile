b679d19be8ab3a37a42622b110f07c4e8714e681
Pei4 <pei4.cats@gmail.com>
2021-04-05 22:04:25 +0800

    try old GAS

--- public/js/lv2.js
+++ public/js/lv2.js
@@ -4,7 +4,7 @@ function send() {
     let test2 = document.querySelector('#t2').value;
     $.ajax({
         type: "get",
-        url: "https://script.google.com/macros/s/AKfycbxyq7f7N1DMjodmJUk0ldT5fZY-vTL2NsKgrOhmQ16NK-Vv8cNe7fOePC55vQ5anrwlkQ/exec",
+        url: "https://script.google.com/macros/s/AKfycbxczugTBSFn2zVudV0D0rf61q7o7WMJ7UVBfAYtZIzsu-aqry0_udg9LA/exec",
         data: {
             "test1": test1,
             "test2": test2
