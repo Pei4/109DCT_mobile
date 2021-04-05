599cd834a22e61d33fcd8b02e85be10005de4c9c
Pei4 <pei4.cats@gmail.com>
2021-04-05 14:27:13 +0800

    update GAS

--- public/js/lv0.js
+++ public/js/lv0.js
@@ -1,15 +1,16 @@
 window.googleDocCallback = function () { return true; };
 let num = 1;
 function read(){
-    console.log('read');
     $.ajax({
-        type: "read",
+        type: "get",
         url: "https://script.google.com/macros/s/AKfycbxczugTBSFn2zVudV0D0rf61q7o7WMJ7UVBfAYtZIzsu-aqry0_udg9LA/exec?callback=googleDocCallback",
         data: {
+            "method":"read",
             "num":num
         },
         success: function(response) {
             console.log(response);
+            console.log('read');
         },
     });
 }
--- public/js/lv2.js
+++ public/js/lv2.js
@@ -4,9 +4,10 @@ function send() {
     let test1 = document.querySelector('#t1').value;
     let test2 = document.querySelector('#t2').value;
     $.ajax({
-        type: "write",
+        type: "get",
         url: "https://script.google.com/macros/s/AKfycbxczugTBSFn2zVudV0D0rf61q7o7WMJ7UVBfAYtZIzsu-aqry0_udg9LA/exec?callback=googleDocCallback",
         data: {
+            "method":"write",
             "test1": test1,
             "test2": test2
         },
