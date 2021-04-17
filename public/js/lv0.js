5a2a623419579b7672f96873bf3b1b0309b3a329
Pei4 <pei4.cats@gmail.com>
2021-04-17 13:48:51 +0800

    update js

--- public/js/lv0.js
+++ public/js/lv0.js
@@ -1,11 +1,10 @@
 window.googleDocCallback = function () { return true; };
-const num = 2;
-function read(){
+function read(e){
     $.ajax({
         type: "get",
         url: "https://script.google.com/macros/s/AKfycbzrScWJX4Jk42ckUKAZ6HKHbBxCR5PZWVnzkHpj2dPBJDaQ5_wvuS3WfYVikkb9kjszHg/exec?callback=googleDocCallback",
         data: {
-            "num":num
+            "num":e+1
         },
         success: function(response) {
             console.log(response);
