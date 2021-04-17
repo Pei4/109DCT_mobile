d3a78754c7f96c0b4b1476fb09c40eaafee840f9
Pei4 <pei4.cats@gmail.com>
2021-04-17 14:03:10 +0800

    update GAS

--- public/js/lv0.js
+++ public/js/lv0.js
@@ -1,10 +1,12 @@
 window.googleDocCallback = function () { return true; };
-function read(e){
+let num = document.querySelector('#getNum');
+function read(){
+    console.log(num);
     $.ajax({
         type: "get",
-        url: "https://script.google.com/macros/s/AKfycbxfYrEDZL_a5ucDguu3T6N1qhavP9J5hJ8ONTUzqvjyJ8cs9SUGBeHrjzSe81yRJ_SKww/exec?callback=googleDocCallback",
+        url: "https://script.google.com/macros/s/AKfycbxglC7c18FzZSSM4dYG_-00OhIKOY6dmIONl3ZGOELAZBM7MGVZpmWjGNrOhKcTQ7VkUQ/exec?callback=googleDocCallback",
         data: {
-            "num":e+1
+            "num":num+1
         },
         success: function(response) {
             console.log(response);
