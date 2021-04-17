f5224179f35900646cac3891eabc13efc84f8b70
Pei4 <pei4.cats@gmail.com>
2021-04-17 16:31:02 +0800

    try write

--- public/js/lv0.js
+++ public/js/lv0.js
@@ -1,20 +1,60 @@
 window.googleDocCallback = function () { return true; };
-let num = document.querySelector('#getNum');
-function read(){
-    console.log(num);
+function callGas(m,v1,v2){
     $.ajax({
         type: "get",
-        url: "https://script.google.com/macros/s/AKfycbxglC7c18FzZSSM4dYG_-00OhIKOY6dmIONl3ZGOELAZBM7MGVZpmWjGNrOhKcTQ7VkUQ/exec?callback=googleDocCallback",
+        url: "https://script.google.com/macros/s/AKfycbz7Zp1HdGHFDSyTU8vx-rE5IYiddFPHlewPtbq5e7XX8H5wYCm54VcNKuvL8BUhVLZyEw/exec?callback=googleDocCallback",
         data: {
-            "num":num+1
+            "method":m,
+            "num":v1,
+            "cate":v2
+        },
+        success: function(response) {
+            if (m == "read"){
+                console.log(response);
+                console.log(m);
+            }
+            else if(m == "write"){
+                console.log(m);
+            }
+        }
+    })
+}
+function read(){
+    let nu = document.querySelector('#getNum').value;
+    let num = parseInt(nu) + 1;
+    console.log(num);
+    callGas("read",num);
+    /*$.ajax({
+        type: "get",
+        url: "https://script.google.com/macros/s/AKfycbz7Zp1HdGHFDSyTU8vx-rE5IYiddFPHlewPtbq5e7XX8H5wYCm54VcNKuvL8BUhVLZyEw/exec?callback=googleDocCallback",
+        data: {
+            "method":"read",
+            "num":num
         },
         success: function(response) {
             console.log(response);
             console.log('read');
         }
-    });
+    });*/
 }
-
+function write(){
+    let nu = document.querySelector('#getNum').value;
+    let nu2 = parseInt(nu) + 1;
+    let cat = document.querySelector('#getCate').value;
+    callGas("write",nu2,cat);
+    /*$.ajax({
+        type: "get",
+        url: "https://script.google.com/macros/s/AKfycbz7Zp1HdGHFDSyTU8vx-rE5IYiddFPHlewPtbq5e7XX8H5wYCm54VcNKuvL8BUhVLZyEw/exec?callback=googleDocCallback",
+        data: {
+            "method":"write",
+            "num":num2,
+            "cate":cate2
+        },
+        success: function (){
+            console.log("write");
+        }
+    })*/
+}
 
 /*function setup(){
     createCanvas(window.innerWidth, window.innerHeight);
