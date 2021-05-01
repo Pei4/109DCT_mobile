e9e2c46dece95334c01fdbbb1027d53c7fb55d72
Pei4 <pei4.cats@gmail.com>
2021-05-01 22:09:43 +0800

    try new GAS

--- public/js/lv2_1.js
+++ public/js/lv2_1.js
@@ -3,7 +3,7 @@ let check = 0;
 function callGas(method,successFnt){
     $.ajax({
         type: "get",
-        url: "https://script.google.com/macros/s/AKfycbykVhu7hk1sWNbZe2n9FQV84mxhxCSrMAA4WphTOpSMsNbUURAfo79jEdgkLI9Jp-1bnQ/exec?callback=googleDocCallback",
+        url: "https://script.google.com/macros/s/AKfycbyfRZEpj626zEXBiERdrAu3v8faaXluQgQUR4Xffye2sTeRXKflK5KoNJcCTuSkeG3Tlw/exec?callback=googleDocCallback",
         data: {
             "method": method,
             "id":parseInt(document.querySelector('#id').value)+1
@@ -18,16 +18,21 @@ let test = function(e){
 };
 let tryCheck = function(e){
     if(e == "retry"){
+        console.log("retry");
         check = 0;
     }
     if(e == "access"){
+        console.log("access");
         check = 1;
     };
 }
 function read() {
     callGas("addPlayer",test);
     for(check !== 1;check += 0;){
-        window.setTimeout(callGas("checkPlayer",tryCheck),2000);
+        window.setTimeout(function(){
+            callGas("checkPlayer",tryCheck);
+            console.log("tryCheck");
+        },2000);
         callGas("addPlayer",test);
     }
 };
