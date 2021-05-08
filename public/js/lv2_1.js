7e1f6e442e6e8107b694844de19efd2ef7e16651
Pei4 <pei4.cats@gmail.com>
2021-05-08 16:31:45 +0800

    fix account

--- public/js/lv2_1.js
+++ public/js/lv2_1.js
@@ -1,25 +1,31 @@
 window.googleDocCallback = function () { return true; };
 let check = 0; // Default/continue 0, overload/almost 1, ready 2
-dialogControl();
+window.onload = function(){
+    dialogControl();
+}
+
 
 function callGas(method,successFnt){
     $.ajax({
         type: "get",
-        url: "https://script.google.com/macros/s/AKfycbzmlZafI4Oec2xMk1qn98XPRBeYp8skmTwt0QxvlGQbiUvv_vhiSHfQrjCyzk19-BMuvA/exec?callback=googleDocCallback",
+        async: false,
+        url: "https://script.google.com/macros/s/AKfycbyI5hk18g3cu6F_hFDxqorwd4axTcfA5hGdjOZ8l70IGGknHbdOoOwmI_D-NeHBUROQlQ/exec?callback=googleDocCallback",
         data: {
             "method": method,
             "id":id+1
         },
         success: function(response) {
             successFnt(response);
+
         }
     });
 }
 
-let test = function(e){
+function test(e){
     console.log(e);
 };
-let tryCheck = function(e){
+function tryCheck(e){
+    console.log(check);
     if(e == "overload"){
         alert("overload, wait for 5 sec");
         check = 1;
@@ -29,40 +35,42 @@ let tryCheck = function(e){
         check = 1;
     }
     if(e == "continue"){
-        alert("connecting");
+        alert("connecting2");
         check = 0;
     }
     if(e == "ready"){
         alert("success");
         check = 2;
     }
+    //return new Promise(resolve => setTimeout(resolve, 10));
 }
-
-function read() {
-    callGas("addPlayer",tryCheck);
+function goCheck(){
+    console.log('goCheck');
     if(check < 2){
         if(check == 0){  //快速檢查是否登錄
-            let connectInternal = setInterval(function (){
-                callGas("addPlayer", tryCheck);
+            console.log('check0');
+            let connectInterval = setInterval(function (){
+                callGas("addPlayer", tryCheck)
                 if(check == 2){
                     console.log("0-2");
-                    clearInterval(connectInternal);
+                    clearInterval(connectInterval);
                 }
                 if(check == 1){
                     console.log("0-1");
-                    clearInterval(connectInternal);
-                    let retryInternal = setInterval(function (){
+                    clearInterval(connectInterval);
+                    let retryInterval = setInterval(function (){
                         callGas("addPlayer", tryCheck);
                         if(check == 2){
                             console.log("0-1-2");
-                            clearInterval(retryInternal);
+                            clearInterval(retryInterval);
                         }
                     },5000);
                 }
             },2000);
         }
         if (check == 1) {  //太多人時過久一點再試
-            let retryInternal = setInterval(function () {
+            let retryInternal = setInterval(async function () {
+                console.log('retry');
                 callGas("addPlayer", tryCheck);
                 if (check == 2){
                     console.log("1-2");
@@ -71,6 +79,12 @@ function read() {
             }, 5000);
         }
     }
+}
+
+function read() {
+    alert('connecting');
+    callGas("addPlayer",tryCheck);
+    goCheck();
 };
 function water(){
     callGas("goWater",test);
