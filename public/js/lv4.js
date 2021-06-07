fd6043f4bc11ae7fb3045efc81a76ff3adf4eae6
Pei4 <pei4.cats@gmail.com>
2021-06-07 23:24:34 +0800

    download function done

--- public/js/lv4.js
+++ public/js/lv4.js
@@ -8,16 +8,30 @@ window.onload = function (){
     );
 }
 
+
 function openLetter(){
     changeSource('letter','../material/letter_after.png');
     document.getElementById('letter').style.transform = 'translate(0,-10%)';
     hideSth('notice');
+    document.getElementById('download').addEventListener('mousedown',()=>{
+        setTimeout(()=>{showSth('doneDownload');},2000);
+    });
+    setTimeout(()=>{
+        addClass('title','shine2');
+        addClass('letter','shine2');
+        addClass('word','show');
+        addClass('photo','show');
+        addClass('end','showBtn');
+        addClass('download','showBtn');
+        showSth('word');
+        showSth('photo');
+        showSth('end');
+        showSth('download');
+    },1000);
     setTimeout(()=>{
         hideSth('title');
         hideSth('letter');
-        showSth('word');
-        showSth('photo');
-    },1000)
+    },2000);
 }
 function browse(){
     showSth('photoMask');
