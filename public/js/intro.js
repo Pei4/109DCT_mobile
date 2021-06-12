71c28bb71ca597a0b49dfb52efab96f933c4f2da
Pei4 <pei4.cats@gmail.com>
2021-06-13 01:46:51 +0800

    try yt

--- public/js/intro.js
+++ public/js/intro.js
@@ -1,12 +1,37 @@
 $(function(){
-    $('#video').css({ width: $(window).innerWidth() + 'px', height: $(window).innerHeight() + 'px' });
+    $('#introVid').css({ width: $(window).innerHeight() + 'px', height: $(window).innerWidth() + 'px' });
     $(window).resize(function(){
-        $('#video').css({ width: $(window).innerWidth() + 'px', height: $(window).innerHeight() + 'px' });
+        $('#introVid').css({ width: $(window).innerHeight() + 'px', height: $(window).innerWidth() + 'px' });
     });
 });
 
 function start(){
     hideSth('main');
     showSth('introVid');
-    document.getElementById('introVid').play();
 }
+
+/*function onYouTubeIframeAPIReady() {
+    let player;
+    player = new YT.Player('YouTubeVideoPlayerAPI', {
+        videoId: 'Xj3gU3jACe8',   // YouTube 影片ID
+        width: newWidth,            // 播放器寬度 (px)
+        height: newHeight,           // 播放器高度 (px)
+        playerVars: {
+            autoplay: 1,            // 自動播放影片
+            controls: 0,            // 顯示播放器
+            showinfo: 0,            // 隱藏影片標題
+            modestbranding: 0,      // 隱藏YouTube Logo
+            loop: 0,                // 重覆播放
+            playlist:'Xj3gU3jACe8', // 當使用影片要重覆播放時，需再輸入YouTube 影片ID
+            fs: 0,                  // 隱藏全螢幕按鈕
+            cc_load_policty: 0,     // 隱藏字幕
+            iv_load_policy: 3,      // 隱藏影片註解
+            autohide: 0             // 影片播放時，隱藏影片控制列
+        },
+        events: {
+            onReady: function(e) {
+                e.target.playVideo(); //強制播放(手機才會自動播放，但僅限於Android)
+            }
+        }
+    });
+}*/
