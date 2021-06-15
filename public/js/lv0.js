a972d913fc809c3e39d82f2f9a668bdc3c7ef0f8
Pei4 <pei4.cats@gmail.com>
2021-06-15 17:41:53 +0800

    clear localStorage

--- public/js/lv0.js
+++ public/js/lv0.js
@@ -1,7 +1,5 @@
 window.googleDocCallback = function () { return true; };
 const planets = [];
-let shineImg,planetImg,planetR,planetW,planetB;
-let btn;
 
 setTimeout(()=>{
     updatePlanets();
@@ -10,6 +8,7 @@ setTimeout(()=>{
         '../material/1.png',
         '../material/2.png'
     )
+    localStorage.clear();
 },1);
 
 //呼叫 Google Apps Script
@@ -48,57 +47,4 @@ class Planet{
         planet.style.transform = 'translate(0,0)';
         document.body.appendChild(planet);
     }
-    /*drawShine(){
-        if(this.shine != 0){
-            shineImg.noFill();
-            shineImg.strokeWeight(1);
-            shineImg.stroke(`rgba(255,255,255,${this.fade})`);
-            shineImg.circle(this.updatePos.x+13,this.updatePos.y+13,this.size);
-            console.log('draw');
-        }
-    }
-    drawPlanet(){
-        image(this.colorString,this.pos.x,this.pos.y,this.size,this.size)
-    }
-    updateShine(){
-        if(this.size < this.shine){
-            this.size = this.size + 3;
-            this.updatePos.add(-3,-3);
-            this.fade = this.fade -(3/this.shine);
-        }
-        else{
-            this.size = 6;
-            this.updatePos = this.pos;
-            this.fade = 1;
-        }
-    }*/
 }
-/*
-//畫圖囉
-function preload(){
-    planetR = loadImage('../material/planet_red.png');
-    planetW = loadImage('../material/planet_white.png');
-    planetB = loadImage('../material/planet_blue.png');
-}
-function setup(){
-    frameRate(30);
-    createCanvas(window.innerWidth,window.innerHeight);
-    shineImg = createGraphics(window.innerWidth,window.innerHeight);
-    planetImg = createGraphics(window.innerWidth,window.innerHeight);
-}
-function draw(){
-    background(223,223,223);
-    planets.forEach(p => {
-        p.drawPlanet();
-        //p.drawShine();
-        //p.updateShine();
-    });
-    //image(shineImg, 0, 0);
-    image(planetImg, 0, 0);
-}
-
-
-//設定光暈物件
-function goEnd(){
-    console.log('end');
-}*/
--- public/js/lv0_arch.js
+++ public/js/lv0_arch.js
@@ -1,4 +1,4 @@
-window.googleDocCallback = function () { return true; };
+/*window.googleDocCallback = function () { return true; };
 let nu;
 let num;
 let cate;
@@ -32,6 +32,60 @@ function writeGas(){
     callGas("write");
 }
 
-/*function setup(){
+function setup(){
     createCanvas(window.innerWidth, window.innerHeight);
+}*/
+
+/*drawShine(){
+     if(this.shine != 0){
+         shineImg.noFill();
+         shineImg.strokeWeight(1);
+         shineImg.stroke(`rgba(255,255,255,${this.fade})`);
+         shineImg.circle(this.updatePos.x+13,this.updatePos.y+13,this.size);
+         console.log('draw');
+     }
+ }
+ drawPlanet(){
+     image(this.colorString,this.pos.x,this.pos.y,this.size,this.size)
+ }
+ updateShine(){
+     if(this.size < this.shine){
+         this.size = this.size + 3;
+         this.updatePos.add(-3,-3);
+         this.fade = this.fade -(3/this.shine);
+     }
+     else{
+         this.size = 6;
+         this.updatePos = this.pos;
+         this.fade = 1;
+     }
+ }
+}
+
+//畫圖囉
+function preload(){
+    planetR = loadImage('../material/planet_red.png');
+    planetW = loadImage('../material/planet_white.png');
+    planetB = loadImage('../material/planet_blue.png');
+}
+function setup(){
+    frameRate(30);
+    createCanvas(window.innerWidth,window.innerHeight);
+    shineImg = createGraphics(window.innerWidth,window.innerHeight);
+    planetImg = createGraphics(window.innerWidth,window.innerHeight);
+}
+function draw(){
+    background(223,223,223);
+    planets.forEach(p => {
+        p.drawPlanet();
+        //p.drawShine();
+        //p.updateShine();
+    });
+    //image(shineImg, 0, 0);
+    image(planetImg, 0, 0);}
+
+
+//設定光暈物件
+function goEnd(){
+    console.log('end');
 }*/
