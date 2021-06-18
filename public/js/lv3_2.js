fd331072a351bbcb8e085991096af968994a2af5
Pei4 <pei4.cats@gmail.com>
2021-06-18 22:32:00 +0800

    try pic

---
+++ public/js/lv3_2.js
@@ -0,0 +1,59 @@
+window.googleDocCallback = function () { return true; };
+//初始設定
+let id = parseInt(localStorage.getItem('id'));
+let iosList = ['iPad Simulator', 'iPhone Simulator', 'iPod Simulator', 'iPad', 'iPhone', 'iPod'];
+let resArray;
+
+setTimeout(()=>{
+    callPlayer();
+    runVid();
+},1);
+
+function callPlayer(){
+    $.ajax({
+        type: "get",
+        async: false,
+        url: "https://script.google.com/macros/s/AKfycbzzGBNH-cmfYl_TCnqYRarZN-1M8pLOTzbKxQr8QzzK3UIqacmeJisiydb5mS8-4s9DLA/exec?callback=googleDocCallback",
+        data: {
+            "id":id+1,
+            "time":-1
+        },
+        success: function(response) {
+            resArray = response.toString().split('');
+        }
+    });
+}
+
+//截圖（拍照）
+function screenshot(){
+    document.getElementById('demoPlanet').src = `../material/p_${resStr}.png`;
+    //影片比例
+    context.fillRect(0, 0, w, h);
+    context.drawImage(vid, 0, 0, w, h);
+    html2canvas(document.getElementById('capture')).then(function(canvas) {
+        if(iosList.includes(navigator.platform)==true|| (navigator.userAgent.includes("Mac") && "ontouchend" in document)==true){
+            let imageURL = canvas.toDataURL();
+            let img = document.getElementById('screenShotImg');
+            img.crossOrigin = "Anonymous";
+            img.id = "getshot";
+            img.src = imageURL;
+            document.getElementById('capture').appendChild(img);
+        }
+        else {
+            document.getElementById('capture').appendChild(canvas);
+            let a = document.createElement('a');
+            a.href = canvas.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
+            a.download = 'photo.jpg';
+            a.click();
+        }
+    });
+    //
+ /*下載
+    let a = document.createElement("a");
+    a.href = getshot.src;
+    a.download = "workout_log.png";
+    a.click();
+    document.getElementById('capture').removeChild(img);*/
+}
+
+
